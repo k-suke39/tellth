effb212cfce70b5e1aa1670a5e1a6f12adc97f29
kesuke390 <ksuke.sakuma@gmail.com>
2023-05-07 15:47:13 +0900

    add

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_07_061911) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_07_064634) do
+  create_table "comments", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "user_id"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title", null: false
     t.string "content", null: false
