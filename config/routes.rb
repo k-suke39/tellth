e9f71f5df5b740fb5f9ab1e53fcb6c75358aaa2f
kesuke390 <ksuke.sakuma@gmail.com>
2023-05-07 15:49:00 +0900

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'comments/index'
+  get 'comments/new'
+  get 'comments/create'
+  get 'comments/delete'
   get 'posts/index'
   get 'posts/new'
   get 'posts/edit'
